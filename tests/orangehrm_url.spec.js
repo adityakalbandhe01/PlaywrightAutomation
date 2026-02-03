1b8b879bc3dbea2b0e2afdaf3d83c671a3047abd
adityakalbandhe01 <adityakalbandhe01@gmail.com>
2026-02-03 18:46:18 +0530

    Created a Playwright Automation framework and added 2 scripts and checked Ttile and url assertion for OrangeHRM demo site

--- tests/orangehrm_url.spec.js
+++ tests/orangehrm_url.spec.js
@@ -3,6 +3,7 @@
 
 const {test, expect} = require('@playwright/test')
 
+
 test('Validate the page title', async({page})=>{
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
